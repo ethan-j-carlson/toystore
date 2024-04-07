c9a0c4128dcc76cf21a18e4b075c8d2f5b62b20f
ethan-j-carlson <98480330+ethan-j-carlson@users.noreply.github.com>
2024-04-07 14:15:50 -0400

    changed user

--- includes/database-connection.php
+++ includes/database-connection.php
@@ -6,7 +6,7 @@ $db       = 'ethancar_toystore';     // Name of the database
 $port     = '3306';                           // Port is usually 3306 in Hostgator
 $charset  = 'utf8mb4';                       // UTF-8 encoding using 4 bytes of data per char
 
-$username = 'me';     // Enter YOUR cPanel username and user here
+$username = 'ethancar_me';     // Enter YOUR cPanel username and user here
 $password = 'Temp_Password123';           // Enter YOUR user password here
 
 
