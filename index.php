85b048b9b6277aca4ee660517054c99e47fbe5d4
ethan-j-carlson <98480330+ethan-j-carlson@users.noreply.github.com>
2024-04-07 14:53:56 -0400

    Cleaned up index.php

--- index.php
+++ index.php
@@ -3,26 +3,11 @@
 	// Include the database connection script
 	require 'includes/database-connection.php';
 
-	/*
-	 * Retrieve toy information from the database based on the toy ID.
-	 * 
-	 * @param PDO $pdo       An instance of the PDO class.
-	 * @param string $id     The ID of the toy to retrieve.
-	 * @return array|null    An associative array containing the toy information, or null if no toy is found.
-	 */
 	function getToy(PDO $pdo, string $id) {
-
-		// SQL query to retrieve toy information based on the toy ID
 		$sql = "SELECT * 
 			FROM toy
-			WHERE toynum= :id;";	// :id is a placeholder for value provided later 
-		                               // It's a parameterized query that helps prevent SQL injection attacks and ensures safer interaction with the database.
-
-
-		// Execute the SQL query using the pdo function and fetch the result
-		$toy = pdo($pdo, $sql, ['id' => $id])->fetch();		// Associative array where 'id' is the key and $id is the value. Used to bind the value of $id to the placeholder :id in  SQL query.
-
-		// Return the toy information (associative array)
+			WHERE toynum= :id;";
+		$toy = pdo($pdo, $sql, ['id' => $id])->fetch();
 		return $toy;
 	}
 
